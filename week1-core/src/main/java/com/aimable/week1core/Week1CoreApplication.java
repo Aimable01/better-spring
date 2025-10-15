dcc78f5d1950e68f31781e8861f4d58e8a8c0533
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-15 11:07:03 +0200

    [init] week1 core auth concepts

---
+++ week1-core/src/main/java/com/aimable/week1core/Week1CoreApplication.java
@@ -0,0 +1,13 @@
+package com.aimable.week1core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week1CoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Week1CoreApplication.class, args);
+    }
+
+}
---
+++ week1-core/src/test/java/com/aimable/week1core/Week1CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aimable.week1core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week1CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

18f148dbad3fc2fa2f00a6b5110e7fe35a3d9eaf
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-15 11:48:55 +0200

    feat: configure inmemory user

---
+++ week1-core/src/main/java/com/aimable/week1core/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.aimable.week1core.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user =  User.withDefaultPasswordEncoder()
+                .username("shema")
+                .password("shema@123")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+}

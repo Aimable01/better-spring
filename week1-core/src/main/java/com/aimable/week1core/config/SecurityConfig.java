450d313944cb003ff8986157d0f74a1d792b9daa
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-15 13:31:26 +0200

    feat: bcrypt password encoder

--- week1-core/src/main/java/com/aimable/week1core/config/SecurityConfig.java
+++ week1-core/src/main/java/com/aimable/week1core/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,11 +16,16 @@ public class SecurityConfig {
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user =  User.withDefaultPasswordEncoder()
-                .username("shema")
-                .password("shema@123")
+        UserDetails user =  User
+                .withUsername("shema")
+                .password(passwordEncoder().encode("shema@1234"))
                 .build();
 
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }

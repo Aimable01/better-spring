a54fbc687b9a9824a910afbb75ccf10f3800f01f
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-15 14:13:13 +0200

    feat: implement UserDetailsService

---
+++ week1-core/src/main/java/com/aimable/week1core/service/CustomUserDetailsService.java
@@ -0,0 +1,39 @@
+package com.aimable.week1core.service;
+
+import com.aimable.week1core.entity.User;
+import com.aimable.week1core.enums.Role;
+import com.aimable.week1core.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),user.getPassword(),
+                getAuthorities(user.getRoles())
+        );
+    }
+
+    private Collection<? extends GrantedAuthority> getAuthorities(Collection<Role> roles) {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                .collect(Collectors.toList());
+    }
+}

66731d28f949eef16d16dd5704f81a38ddcdd454
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-17 16:40:34 +0200

    feat: url based authz and the inmemory users with roles

--- week1-core/src/main/java/com/aimable/week1core/config/SecurityConfig.java
+++ week1-core/src/main/java/com/aimable/week1core/config/SecurityConfig.java
@@ -9,7 +9,11 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -27,28 +31,59 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/auth/register", "/auth/login").permitAll()
+                        // public endpoints
+                        .requestMatchers("/auth/register", "/public/**").permitAll()
+
+                        // role based authz
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/moderator/**").hasAnyRole("MODERATOR","ADMIN")
+                        .requestMatchers("/api/users/**").hasAuthority("USER_READ")
+
+                        // authority based authz
+                        .requestMatchers("/api/posts/create").hasAuthority("POST_CREATE")
+                        .requestMatchers("/api/posts/delete/**").hasAuthority("POST_DELETE")
+
+                        // authenticated but no role required
+                        .requestMatchers("/api/dashboard","/api/profile").authenticated()
+
                         .anyRequest().authenticated()
                 )
-                .userDetailsService(customUserDetailsService)
+//                .userDetailsService(customUserDetailsService)
                 .httpBasic(Customizer.withDefaults())
-                .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/register", "/auth/login"))
-                .addFilterBefore(new DebugFilter(), BasicAuthenticationFilter.class)
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/auth/**", "/public/**","/api/**"))
+//                .addFilterBefore(new DebugFilter(), BasicAuthenticationFilter.class)
                 .build();
     }
 
-    /**
+
     // when configuring in-memory users
      @Bean
      public UserDetailsService userDetailsService() {
-     UserDetails user =  User
-     .withUsername("shema")
-     .password(passwordEncoder().encode("shema@1234"))
-     .build();
 
-     return new InMemoryUserDetailsManager(user);
+     UserDetails adminUser = User.builder()
+             .username("admin")
+             .password(passwordEncoder().encode("admin@123"))
+             .roles("ADMIN")
+             .authorities("USER_READ","USER_WRITE","POST_READ","POST_WRITE","POST_DELETE")
+             .build();
+
+         UserDetails moderatorUser = User.builder()
+                 .username("moderator")
+                 .password(passwordEncoder().encode("moderator@123"))
+                 .roles("MODERATOR")
+                 .authorities("USER_READ", "POST_READ", "POST_WRITE")
+                 .build();
+
+         UserDetails regularUser = User.builder()
+                 .username("user")
+                 .password(passwordEncoder().encode("moderator@123"))
+                 .roles("USER")
+                 .authorities("POST_READ")
+                 .build();
+
+     return new InMemoryUserDetailsManager(adminUser, moderatorUser, regularUser);
      }
-     */
+
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
--- week1-core/src/main/java/com/aimable/week1core/enums/Role.java
+++ week1-core/src/main/java/com/aimable/week1core/enums/Role.java
@@ -2,5 +2,6 @@ package com.aimable.week1core.enums;
 
 public enum Role {
     USER,
-    ADMIN
+    ADMIN,
+    MODERATOR
 }

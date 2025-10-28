55d085cfda37dc1570e373940c94e2e0bfb65f28
Aimable01 <aimable.kwizera14@gmail.com>
2025-10-28 19:43:31 +0200

    [init] week2 jwt init

--- week2-jwt/src/main/java/com/aimable/week2jwt/Week2JwtApplication.java
+++ week2-jwt/src/main/java/com/aimable/week2jwt/Week2JwtApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Week2JwtApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(Week2JwtApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Week2JwtApplication.class, args);
+	}
 
 }
--- week2-jwt/src/test/java/com/aimable/week2jwt/Week2JwtApplicationTests.java
+++ week2-jwt/src/test/java/com/aimable/week2jwt/Week2JwtApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Week2JwtApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
